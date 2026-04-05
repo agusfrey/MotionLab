7168f6e8bcef7f8cc550d79bf4c814cf1cd1707b
agusfrey <afrey@udesa.edu.ar>
2026-04-05 19:50:29 -0300

    Update metricas.py

--- src/metricas.py
+++ src/metricas.py
@@ -39,35 +39,4 @@ def calcular_tiempo_primer_hit(datos):
     return None  # si no hubo ningún hit
 
 
-# OPCIONAL
-def calcular_hits_por_bloques(datos, ventana):
-    """
-    Cuenta los hits ocurridos en cada bloque de tiempo de duración fija.
 
-    Divide la serie temporal en bloques de tamaño igual a ventana (en segundos)
-    y cuenta cuántos hits ocurren dentro de cada bloque.
-
-    Parámetros:
-        datos (dict): diccionario de un participante con las claves "hit" 
-                      y "tiempo", ambas listas del mismo largo.
-        ventana (float): duración en segundos de cada bloque de tiempo.
-
-    Retorna:
-        list: lista de enteros, donde cada elemento representa 
-              la cantidad de hits en ese bloque de tiempo.
-    """
-    bloques = []
-    tiempo  = datos["tiempo"]
-    hits    = datos["hit"]
-    tiempo_inicio = tiempo[0]
-    hits_bloque   = 0
-    for i in range(len(tiempo)):
-        if tiempo[i] - tiempo_inicio < ventana:
-            if hits[i] == True:
-                hits_bloque = hits_bloque + 1
-        else:
-            bloques.append(hits_bloque)
-            tiempo_inicio = tiempo[i]
-            hits_bloque   = 1 if hits[i] == True else 0
-    bloques.append(hits_bloque)  # agrega el último bloque
-    return bloques
