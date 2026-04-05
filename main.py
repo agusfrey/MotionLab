2e112f864135d46aecc2ab968afc8e13db0da34c
agusfrey <afrey@udesa.edu.ar>
2026-04-05 19:18:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from src.carga_datos import cargar_datos
 from src.validacion_datos import validar_registro
 from src.metricas import calcular_hits_totales, calcular_tiempo_primer_hit
 
-# 1. Cargar datos desde el archivo
+# 1. cargar datos desde el archivo
 datos = cargar_datos("datos/datos.csv")
 
-# 2. Filtrar datos válidos
+# 2. filtrar datos válidos
 datos_validos = []
 
 for registro in datos:
     if validar_registro(registro):
         datos_validos.append(registro)
 
-# 3. Calcular métricas y mostrar resultados
+# 3. calcular métricas y mostrar resultados
 for registro in datos_validos:
     id_p       = registro["id_participante"]
     condicion  = registro["condicion"][0]
